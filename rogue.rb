f86cf749acc4f5a4f74b5f5cbcb0cd4e7f7680da
Chad Jemmett <chad.jemmett@gmail.com>
2017-05-22 19:58:02 -0600

    tested the code

--- rogue.rb
+++ rogue.rb
@@ -81,8 +81,9 @@ class GameWindow < Gosu::Window
     Gosu::translate(-@camera_x, -@camera_y) do
       @map.each {|tile| tile.draw(@player.x, @player.y)}
       @player.draw
-      #@text.draw("X: #{@player.x} Y: #{@player.y}", @player.x - 60, @player.y - 15, 1)
+      @text.draw("X: #{@player.x} Y: #{@player.y}", @player.x - 60, @player.y - 15, 1)
     end
+#we'll draw the message box outside the camera
       #@text.draw("Cam X: #{-@camera_x} Cam Y: #{@camera_y}", 200, 200, 1)
 
   end
